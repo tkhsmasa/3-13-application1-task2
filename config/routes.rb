0527f64bec7d9a3b5f08caabaab57df21868f2b9
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-11-06 10:47:42 +0000

    [add]ルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  get '/top' => 'homes#top'
+  get '/books' => 'books#top'
+  get '/books/new' =>'books#new'
+  get 'books/index' =>'books#index'
+  get 'books/show' =>'books#show'
+  get 'books/edit' =>'books#edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
