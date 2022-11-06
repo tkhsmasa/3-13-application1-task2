0184afb33d09891a2156e6d4175cd1c758761e4a
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-11-06 11:51:41 +0000

    [add]indexの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  get '/books' => 'books#top'
-  get '/books/new' =>'books#new'
+  get 'books/new' =>'books#new'
+  get 'top' => 'homes#top'
+  get 'books/top' => 'books#top'
   post 'books' => 'books#create'
-  get 'books/index' =>'books#index'
-  get 'books/show' =>'books#show'
-  get 'books/edit' =>'books#edit'
+  get 'books/index' => 'books#index'
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
