c4b47902d1203887f639c8ff8c520124f577f110
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-11-07 14:47:05 +0000

    [add]編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new' =>'books#new'
   get 'top' => 'homes#top'
-  get 'books/top' => 'books#top'
   post 'books' => 'books#create'
-  get 'books/index' => 'books#index'
-  get 'books/:id' => 'books#show',as:'book'
-  get 'books/edit' => 'books#edit'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
