bea2d20ab5419324100ebf2503dba312059728f1
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-11-07 15:08:15 +0000

    [add]削除機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,11 +21,17 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book= Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
   end
 
   private
