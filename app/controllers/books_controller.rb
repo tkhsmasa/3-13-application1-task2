b51fc85d789e0bd0c0a0652678c685411c67149c
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-11-27 04:45:00 +0000

    [add]rootpath

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
     end
-
   end
 
   def index
@@ -30,8 +30,14 @@ class BooksController < ApplicationController
 
   def update
     book= Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book= Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top' => 'homes#top'
+  root to:  'homes#top'
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
